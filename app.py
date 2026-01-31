886043f2e0a889cf557968e4eead7e3ddf651192
VarshaKM810 <varshakm302@gmail.com>
2026-01-31 12:41:46 +0530

    name

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+
+def index():
     return render_template("index.html")
+
+@app.route("/<name>")
+def name(name):
+    return render_template("name.html",name=name)
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
 if __name__ == "__main__":
     app.run(debug=True)
